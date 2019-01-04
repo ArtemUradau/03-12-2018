b25806be0b01b19f174bf5b176c8f94876f4f8c5
Olga R <olga.rondarava@oxagile.com>
2019-01-04 18:31:27 +0300

    no message

--- B-6(5)-Arrays-Strings/B-6-Arrays-Strings/Program.cs
+++ B-6(5)-Arrays-Strings/B-6-Arrays-Strings/Program.cs
@@ -10,10 +10,89 @@ namespace Base.Lesson_6
     {
         static void Main(string[] args)
         {
-           
+            //int[] six = new[] {1, 2, 3, 4, 5, 6};
+            //for (int i = six.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(six[i]);
+            //}
+            //Console.WriteLine(string.Join(", ", six));
+            Random rnd = new Random();
+            int [,] six2 = new int[3,4];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    six2[i, j] = rnd.Next(100);
+                }
+            }
+
+            outmass(six2);
+            int temp = 0;
+            for (int i = 0; i < six2.GetLength(0); i++)
+            {
+                temp = 0;
+                for (int j = 0; j < six2.GetLength(1); j++)
+                {
+                    if (six2[i,j] >= temp)
+                    {
+                        temp = six2[i, j];
+                    }
+                }
+                
+                Console.WriteLine(temp);
+            }
+
+            //PoemGeneration();
             Console.ReadLine();
         }
 
+        public static void PoemGeneration()
+        {
+            Random rnd = new Random();
+            char[][] poem = new char[100][];
+            
+            for (int i = 0; i <= 99; i++)
+            {
+                var word = new char[rnd.Next(10)];
+                for (int j = 0; j < word.Length; j++)
+                {
+                    word[j] = (char)rnd.Next(1040 - 1, 1103 - 1);
+                }
+
+                poem[i] = word;
+                
+            }
+
+            foreach (var word in poem)
+            {
+                Console.WriteLine(word);
+            }
+
+            //for (int i = 0; i < poem.Length; i++)
+            //{
+            //    Console.WriteLine(poem[i]);
+            //}
+            
+
+        }
+        public static void outmass(int [,] six2)
+        {
+            //for (int i = 0; i < (six2.GetUpperBound(0) + 1); i++)
+            //{
+
+            //    for (int j = 0; j < six2.Length / (six2.GetUpperBound(0) + 1); j++)
+            //    {
+                    
+            //        Console.Write($"{six2[i, j], 5}");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+            foreach (var element in six2)
+            {
+                Console.WriteLine(element);
+            }
+        }
         public static void Pyatnashki()
         {
            
--- B-7(6)-Collections/B-7-Collection/Program.cs
+++ B-7(6)-Collections/B-7-Collection/Program.cs
@@ -12,11 +12,39 @@ namespace Base.Lesson_5
         static void Main(string[] args)
         {
             //ArrayListExample();
+            ArrayListExample2();
 
             Console.ReadLine();
         }
 
 
+        static void ArrayListExample2()
+        {
+            var list = new List<Song>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Next row please");
+                var song = new Song()
+                {
+                    Lyrics = Console.ReadLine()
+                };
+
+                list.Add(song);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            list.Sort();
+            list.RemoveAt(list.Count - 1);
+
+            foreach (var row in list)
+            {
+                Console.WriteLine(row.Lyrics);
+            }
+        }
 
         public static void ArrayListExample()
         {
@@ -37,16 +65,23 @@ namespace Base.Lesson_5
             {
                 Console.WriteLine(item);
             }
-
         }
 
-        public class Song
-        {
-            public string Lyrics;
 
-            public override string ToString()
+        public class Song : IComparable
+        {
+            public string Lyrics { get; set; }
+
+            public int CompareTo(object obj)
             {
-                return this.Lyrics;
+                var song = obj as Song;
+
+                if (song.Lyrics == this.Lyrics)
+                {
+                    return 0;
+                }
+
+                return song.Lyrics.CompareTo(this.Lyrics);
             }
         }
     }
